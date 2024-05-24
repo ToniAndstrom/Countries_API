dadeb155947acd3cefa0e6f7f027c5bd13da3800
Toni Andström <toni_andstrom@hotmail.com>
2024-05-24 14:29:31 +0300

    added the weather part to Controller , Twig and Service. Work in progress.

--- src/Service/CallApiServiceCountry.php
+++ src/Service/CallApiServiceCountry.php
@@ -4,7 +4,7 @@ namespace App\Service;
 
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
-class CallApiService
+class CallApiServiceCountry
 {
     private $httpClient;
     public function __construct(HttpClientInterface $httpClient)
